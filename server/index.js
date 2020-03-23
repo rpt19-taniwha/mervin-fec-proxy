616157a73913b7fce9224c51523febb4e0fb7ff4
Mervin Pan <mervinpan@gmail.com>
2020-03-22 17:46:52 -0700

    Added boilerplate for index files

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// SERVER
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/../public/')));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port);
